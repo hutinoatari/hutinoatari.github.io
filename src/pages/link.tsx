0ddacaefe8e15d03eadbe7611fec617921fbb5de
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2021-09-12 19:53:31 +0900

    リンクを追加した

--- src/pages/link.tsx
+++ src/pages/link.tsx
@@ -13,7 +13,61 @@ const LinkPage: FC<{}> = () => {
                 <h2>LINK</h2>
                 <ul>
                     <li>
-                        <ExternalLink title="" url="" />
+                        <ExternalLink
+                            title="こちらに用意がございます。"
+                            url="https://characordbracelets.hateblo.jp/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="わたとか"
+                            url="https://m393.web.fc2.com/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="つまみネット"
+                            url="https://trpfrog.net/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="あずきノート"
+                            url="https://uxhpu.net/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="B.B.B. World"
+                            url="https://baki-0.github.io/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="ねぎー日記"
+                            url="https://negiissei.com/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="Monbrand Software"
+                            url="https://www.mbsoftware.tokyo/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink title="ごっち" url="https://gotti.dev/" />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="りんりん"
+                            url="https://lnln.dev/"
+                        />
+                    </li>
+                    <li>
+                        <ExternalLink
+                            title="アイスの棒"
+                            url="https://azukibar.dev/"
+                        />
                     </li>
                 </ul>
             </main>
