2a716db107a8be901e39a94877b2aea88f94984c
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2021-10-02 22:08:27 +0900

    サイト名を中央に配置した

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,25 @@
 import { FC } from "react";
 import Link from "next/link";
+import styles from "../styles/Header.module.scss";
 
 const Header: FC<{}> = () => {
     return (
         <header>
-            <h1>捻れたバベル</h1>
-            <Link href="/">
-                <a>TOP</a>
-            </Link>{" "}
-            <Link href="/about">
-                <a>ABOUT</a>
-            </Link>{" "}
-            <Link href="/gallery">
-                <a>GALLERY</a>
-            </Link>{" "}
-            <Link href="/link">
-                <a>LINK</a>
-            </Link>
+            <h1 className={styles.title}>捻れたバベル</h1>
+            <nav>
+                <Link href="/">
+                    <a>TOP</a>
+                </Link>{" "}
+                <Link href="/about">
+                    <a>ABOUT</a>
+                </Link>{" "}
+                <Link href="/gallery">
+                    <a>GALLERY</a>
+                </Link>{" "}
+                <Link href="/link">
+                    <a>LINK</a>
+                </Link>
+            </nav>
         </header>
     );
 };
