3402aad8e0dbab121339c1697329cad4ce83bc87
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-04-05 01:49:56 +0900

    マークダウンの処理の仕方を変更

--- src/libs/cheeseTownForBlog.ts
+++ src/libs/cheeseTownForBlog.ts
@@ -1,4 +1,4 @@
-const regex = /^\[(.+)\] (.+)/;
+const regex = /^\[section\] (.+)/;
 
 const mdToHtml = (markdown: string): string => {
     const escapedAnglebracketsMarkdown: string = markdown.replace(
@@ -6,25 +6,10 @@ const mdToHtml = (markdown: string): string => {
         (c) => (c === "<" ? "&lt;" : "&gt;")
     );
     const lines: string[] = escapedAnglebracketsMarkdown.split("\n");
-    const linesWithoutBlankline: string[] = lines
-        .map((line) => line.trim())
-        .filter((line) => line !== "");
-    const elements: string[] = linesWithoutBlankline.map((line) => {
+    const elements: string[] = lines.map((line) => {
         const match = line.match(regex);
-        if (match === null) return `<p>${line}</p>`;
-        const elementName = match[1];
-        const text = match[2];
-        switch (elementName) {
-            case "section":
-                return `<h3>${text.trim()}</h3>`;
-            case "list":
-                const items: string[] = text.split("|");
-                return `<ul>${items.map(
-                    (item) => `<li>${item.trim()}</li>`
-                )}</ul>`;
-            default:
-                return `<p>${line}</p>`;
-        }
+        if(match) return `<h3>${match[1]}</h3>`;
+        return `<p>${line}</p>`;
     });
     const html = elements.join("");
     return html;
