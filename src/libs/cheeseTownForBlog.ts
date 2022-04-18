82b6d50dc6b08d6112feaa2ea57489aca9688703
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-04-18 20:30:13 +0900

    リストの記法を変更

--- src/libs/cheeseTownForBlog.ts
+++ src/libs/cheeseTownForBlog.ts
@@ -11,9 +11,9 @@ const parser: Syntax[] = [
         },
     },
     {
-        regex: /^\[list\](.+)/,
-        convert: (_, text) => {
-            const items = text.split("|");
+        regex: /^\[list\]{(.)}(.+)/,
+        convert: (_, separater, text) => {
+            const items = text.split(separater);
             return `<ul>${items
                 .map((item) => `<li>${item.trim()}</li>`)
                 .join("")}</ul>`;
