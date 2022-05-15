85d23ce556fb2a02a67edebbe0cc0fc4dd851de3
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-05-15 19:25:00 +0900

    404ページを追加

---
+++ routes/_404.tsx
@@ -0,0 +1,14 @@
+import { Head } from "aleph/react";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Error | 捻れたバベル</title>
+            </Head>
+            <p>存在しないページです。</p>
+        </>
+    );
+};
+
+export default ErrorPage;
