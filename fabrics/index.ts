7034d7dc7e8de71c281e5dc6c37076f94ae448ea
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-05-26 03:16:18 +0900

    本番環境デバッグ

--- fabrics/index.ts
+++ fabrics/index.ts
@@ -1,7 +1,7 @@
 import { Fabric } from "../type.ts";
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { WorkListResponse, WorkResponse } from "../types/api.ts";
+import { WorkListResponse } from "../types/api.ts";
 
 const TopPage: Fabric = async () => {
     const document = new DOMParser().parseFromString("", "text/html");
@@ -22,8 +22,11 @@ const TopPage: Fabric = async () => {
         },
     );
     const res = await fetch(req);
-    const json = await res.json();
-    const contents: WorkResponse[] = json.contents ?? [];
+    console.log(res);
+    const json: WorkListResponse = await res.json();
+    console.log(json);
+    const contents = json.contents ?? [];
+    console.log(contents)
     const ul = document.createElement("ul");
     for (const content of contents) {
         const li = document.createElement("li");
