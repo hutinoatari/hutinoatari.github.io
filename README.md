0659ca5c095b204db8dc4ac8943599acf2f43068
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2024-07-02 02:26:43 +0900

    destroy homepage

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# hutinoatari-blog
-
-`curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.38.0 && /vercel/.deno/bin/deno run --allow-read --allow-write --importmap=import_map.json ./build.ts`
+# hutinoatari.github.io
--- loom.ts
+++
@@ -1,4 +0,0 @@
-const cover = (head: string, body: string) =>
-    `<!DOCTYPE html><html lang="ja"><head>${head}</head><body><header><h1>淵野アタリ</h1><nav><ul><li><a href="./index.html">概要</a></li><li><a href="./work.html">作品</a></li></ul></nav></header><main>${body}</main><footer><p><small>&copy;2016 淵野アタリ</small></p></footer></body></html>`;
-
-export { cover };
